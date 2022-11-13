053e38eba9a23329a983fb4f24dc4086f855db32
huscker <egostalker03@mail.ru>
2022-11-13 17:09:09 +0300

    Added new variables for keycloak

--- app/settings.py
+++ app/settings.py
@@ -14,5 +14,14 @@ DB_PASSWORD = os.getenv('DB_PASSWORD','postgres')
 DB_PORT = os.getenv('DB_PORT','5432')
 DB_HOST = os.getenv('DB_HOST','localhost')
 
+# Keycloak
+KEYCLOAK_PORT = os.getenv("KEYCLOAK_PORT", 8888)
+REALM_NAME = os.getenv("REALM_NAME", "parking-map")
+KEYCLOAK_URL = os.getenv("KEYCLOAK_URL", f"https//localhost:{KEYCLOAK_PORT}")
+KEYCLOAK_CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID", "")
+KEYCLOAK_CLIENT_SECRET = os.getenv("KEYCLOAK_CLIENT_SECRET", "")
+KEYCLOAK_ADMIN_USER = os.getenv("KEYCLOAK_ADMIN_USER", "admin")
+KEYCLOAK_ADMIN_PASSWORD = os.getenv("KEYCLOAK_ADMIN_PASSWORD", "admin")
+
 DB_CONNECTION_STRING = f'postgres://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 REDIS_CONNECTION_STRING = ''
