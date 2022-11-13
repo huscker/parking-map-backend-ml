f64c038f503f18b2e417a93bbf09f94187273f41
huscker <egostalker03@mail.ru>
2022-11-13 17:08:56 +0300

    Added auth models

---
+++ app/views/auth.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: Optional[str]
+    expires_in: Optional[int]
+    refresh_expires_in: Optional[int]
+    token_type: Optional[str]
+
+class UserIn(BaseModel):
+    name: str
+    given_name: str
+    family_name: str
+    preferred_username: str
+    email: str
+    password: str
+
+class UserInfo(UserIn):
+    email_verified: bool
+    password: Optional[str] = ''
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
